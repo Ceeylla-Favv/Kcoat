dd884c57de6c80c874254dbc10262ea98a9a789e
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-03-22 18:07:00 +0100

    changes to product file

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -1,37 +1,37 @@
 // productRouter.js
 //importing controllers for review and products
-const productController = require('../controllers/productController.js');
-const reviewController = require("../controllers/reviewController.js")
+const {addProduct, getAllProducts, getOneProduct, updateProduct, deleteProduct, getPublishedProduct, getProductReviews} = require('../controllers/productController.js');
+const {addReview,getAllReviews} = require("../controllers/reviewController.js");
 
 //import router
 const router = require('express').Router();
 
 
 //use routers
-router.post('/addProduct', productController.addProduct);
+router.post('/addProduct', addProduct);
 
-router.get('/allProduct', productController.getAllProducts);
+router.get('/allProduct', getAllProducts);
 
-router.get('/published', productController.getPublishedProduct);
+router.get('/published', getPublishedProduct);
 
 
 //Review url and Controllers
 
-router.post('/addReview', reviewController.addReview)
-router.get('/allReviews', reviewController.getAllReviews)
+router.post('/addReview', addReview)
+router.get('/allReviews', getAllReviews)
 
 
 // get product Reviews
-router.get('/getProductReviews', productController.getProductReviews)
+router.get('/getProductReviews', getProductReviews)
 
 
 
 
 //products router
-router.get('/:id', productController.getOneProduct);
+router.get('/:id', getOneProduct);
 
-router.put('/:id', productController.updateProduct);
+router.put('/:id', updateProduct);
 
-router.delete('/:id', productController.deleteProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
